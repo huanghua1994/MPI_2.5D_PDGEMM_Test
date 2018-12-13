44b27fa1075a37ef2b5b4feae22661f8204aad69
Hua Huang <huangh223@gatech.edu>
2018-12-13 15:18:33 -0500

    Change some variable names

--- MM25D.c
+++ MM25D.c
@@ -10,75 +10,93 @@
 
 #include "utils.c"
 
-
-int main(int argc, char* argv[]) 
+// Factor nproc = c * nproc_ij * nproc_ij where c is a factor of nproc_ij
+void get_nproc_ij_c(int argc, char **argv, int *_nproc, int *_my_rank, int *_c, int *_nproc_ij)
 {
-    int me; /* rank of process */
-    int p; /* number of processes */
-    int root = 0; /* rank of sender */
-    int tag = 0; /* tag for messages */
-    MPI_Status status; /* return status for receive */
-
-    /* start up MPI */
-    MPI_Init(&argc, &argv);
-
-    /* find out process rank */
-    mpi_check(MPI_Comm_rank(MPI_COMM_WORLD, &me));
-    /* find out number of processes */
-    mpi_check(MPI_Comm_size(MPI_COMM_WORLD, &p));
-
-    /*p_gridsize * p_gridsize * c = p */
-    /* c is in argv[2]*/
-    int c, p_gridsize;
-    if(argc > 2) {
+    int my_rank, nproc;
+    int c, nproc_ij, succ = 1;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    
+    if (argc > 2) 
+    {
         c = atoi(argv[2]);
-        p_gridsize = (int)sqrt(p/c);
+        nproc_ij = (int) sqrt(nproc / c);
     } else {
-        p_gridsize = (int)sqrt(p/2);
-        c = p/(p_gridsize * p_gridsize);
+        nproc_ij = (int) sqrt(nproc / 2);
+        c = nproc / (nproc_ij * nproc_ij);
     }
-    if( p_gridsize * p_gridsize * c != p) {
-      if (me == root){
-          fprintf(stderr, "[ERROR]P=%d, the chosen c=%d, cannot decide p_gridsize to meet c*(p_gridsize)^2=p!!\n", p, c);
-      }
-      MPI_Finalize();
-      exit(1);
+    
+    if (nproc_ij * nproc_ij * c != nproc) 
+    {
+        if (my_rank == 0)
+            fprintf(stderr, "[ERROR] nproc = %d, c = %d, cannot decide nproc_ij satisfy c * (nproc_ij)^2 = nproc!!\n", nproc, c);
+        succ = 0;
     }
-    if( c > p_gridsize ) {
-        if (me == root){
-            fprintf(stderr, "[ERROR]c=%d is larger than p_gridsize=%d!\n", c, p_gridsize);
-        }
+    
+    if (c > nproc_ij) 
+    {
+        if (my_rank == 0)
+            fprintf(stderr, "[ERROR] c = %d is larger than nproc_ij = %d !\n", c, nproc_ij);
+        succ = 0;
+    }
+    
+    if (nproc_ij % c != 0)
+    {
+        if (my_rank == 0)
+            fprintf(stderr, "[ERROR] c = %d is not a factor of nproc_ij = %d !\n", c, nproc_ij);
+        succ = 0;
+    }
+    
+    if (succ == 0)
+    {
         MPI_Finalize();
         exit(1);
     }
+    
+    *_nproc = nproc;
+    *_my_rank = my_rank;
+    *_c = c;
+    *_nproc_ij = nproc_ij;
+}
 
-    int n = get_problem_size(argc, argv, p_gridsize, me);
-    int n_local = n / p_gridsize;
-    if(me == root) {printf("[MMM25D]P=%d, n=%d, p_gridsize=%d, c=%d\n", p, n, p_gridsize, c);}
+int main(int argc, char* argv[]) 
+{
+    int root = 0;
+    MPI_Status status; 
+
+    MPI_Init(&argc, &argv);
+    
+    int my_rank, nproc, c, nproc_ij;
+    get_nproc_ij_c(argc, argv, &nproc, &my_rank, &c, &nproc_ij);
+
+    int n = get_problem_size(argc, argv, nproc_ij, my_rank);
+    int n_local = n / nproc_ij;
+    int local_bs = n_local * n_local;
+    if (my_rank == 0) printf("[MMM25D] nproc = %d, n = %d, nproc_ij = %d, c = %d\n", nproc, n, nproc_ij, c);
 
     //prepare for the cartesian topology
     MPI_Comm cartcomm;
-    int dims[3] = {p_gridsize, p_gridsize, c};
+    int dims[3] = {nproc_ij, nproc_ij, c};
     int periods[3] = {1,1,0}, reorder=0,coords[3];
     mpi_check(MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, reorder, &cartcomm));
-    MPI_Comm_rank(cartcomm, &me);
-    MPI_Cart_coords(cartcomm, me, 3, coords);
-
-
+    MPI_Comm_rank(cartcomm, &my_rank);
+    MPI_Cart_coords(cartcomm, my_rank, 3, coords);
 
     //Split the cartcomm into rowcomm, colcomm
     MPI_Comm rowcomm, colcomm, depthcomm;
-    MPI_Comm_split(cartcomm, coords[1]*p_gridsize+coords[2], coords[0], &colcomm); //i as rank
-    MPI_Comm_split(cartcomm, coords[0]*p_gridsize+coords[2], coords[1], &rowcomm); //j as rank
-    MPI_Comm_split(cartcomm, coords[0]*p_gridsize+coords[1], coords[2], &depthcomm); //k as rank
+    MPI_Comm_split(cartcomm, coords[1]*nproc_ij+coords[2], coords[0], &colcomm); //i as rank
+    MPI_Comm_split(cartcomm, coords[0]*nproc_ij+coords[2], coords[1], &rowcomm); //j as rank
+    MPI_Comm_split(cartcomm, coords[0]*nproc_ij+coords[1], coords[2], &depthcomm); //k as rank
 
     MPI_Comm dim_ij_comm; //used for scatter data and collect result
-    MPI_Comm_split(cartcomm, coords[2], coords[0]*p_gridsize+coords[1], &dim_ij_comm); //ij as rank
+    MPI_Comm_split(cartcomm, coords[2], coords[0]*nproc_ij+coords[1], &dim_ij_comm); //ij as rank
 
     double *M, *NT, *P;
     int root_coords[3] = {0,0,0}; //with zero first
     MPI_Cart_rank(cartcomm, root_coords, &root); //get the dst
-    if (root == me) {
+    if (root == my_rank) {
         initial_matrix(&M, &NT, &P, n, n, n);
     }
 
@@ -92,12 +110,12 @@ int main(int argc, char* argv[])
 
     //Scatter and Gather used data types
     MPI_Datatype subarrtype;
-    int sendcounts[p_gridsize*p_gridsize];
-    int displs[p_gridsize*p_gridsize]; //value in block (resized) count
-    init_subarrtype(root, me, n, p_gridsize, n_local, &subarrtype, sendcounts, displs);
+    int sendcounts[nproc_ij*nproc_ij];
+    int displs[nproc_ij*nproc_ij]; //value in block (resized) count
+    init_subarrtype(root, my_rank, n, nproc_ij, n_local, &subarrtype, sendcounts, displs);
 
     //Now A/BT local are ready accorss layer 0.
-    scatter_data(root, me, coords, n, p_gridsize, n_local,
+    scatter_data(root, my_rank, coords, n, nproc_ij, n_local,
             dim_ij_comm, sendcounts, displs, subarrtype,
             A, BT, M, NT);
 
@@ -114,19 +132,19 @@ int main(int argc, char* argv[])
     int dst, src; //use for shift
     int datatag = 0;
     //For the i-th row of the subtasks grid the matrix  A blocks are shifted for (i-1) positions to the left,
-    int shift = coords[0] + coords[2] * p_gridsize / c;
+    int shift = coords[0] + coords[2] * nproc_ij / c;
     if(shift > 0) {
-        dst = (coords[1] +  c*p_gridsize - shift) % p_gridsize;
-        src = (coords[1] + shift) % p_gridsize;
+        dst = (coords[1] +  c*nproc_ij - shift) % nproc_ij;
+        src = (coords[1] + shift) % nproc_ij;
 
         MPI_Sendrecv_replace(A, n_local*n_local, MPI_DOUBLE, dst,
         datatag, src, datatag, rowcomm, &status);
     }
 
-    shift = coords[1] + coords[2] * p_gridsize / c;
+    shift = coords[1] + coords[2] * nproc_ij / c;
     if(shift > 0) {
-        dst = (coords[0] +  c*p_gridsize - shift) % p_gridsize;
-        src = (coords[0] + shift) % p_gridsize;
+        dst = (coords[0] +  c*nproc_ij - shift) % nproc_ij;
+        src = (coords[0] + shift) % nproc_ij;
 
         MPI_Sendrecv_replace(BT, n_local*n_local, MPI_DOUBLE, dst,
         datatag, src, datatag, colcomm, &status);
@@ -136,10 +154,10 @@ int main(int argc, char* argv[])
     //finish initial shift
 
 
-    int j_dst = (coords[1] +p_gridsize - 1) % p_gridsize; //used for BT's shift
-    int j_src = (coords[1] + 1) % p_gridsize; //used for BT's shift
-    int i_dst = (coords[0] +p_gridsize - 1) % p_gridsize; //used for BT's shift
-    int i_src = (coords[0] + 1) % p_gridsize; //used for BT's shift
+    int j_dst = (coords[1] +nproc_ij - 1) % nproc_ij; //used for BT's shift
+    int j_src = (coords[1] + 1) % nproc_ij; //used for BT's shift
+    int i_dst = (coords[0] +nproc_ij - 1) % nproc_ij; //used for BT's shift
+    int i_src = (coords[0] + 1) % nproc_ij; //used for BT's shift
 
     //reference: http://www.hpcc.unn.ru/mskurs/ENG/PPT/pp08.pdf
 
@@ -149,7 +167,7 @@ int main(int argc, char* argv[])
             n_local, n_local, n_local,
             1, A, n_local, BT, n_local, 0, C, n_local);
 
-    for (stage++; stage < p_gridsize/c; stage++) 
+    for (stage++; stage < nproc_ij/c; stage++) 
     {
         /* Send submatrix of A left and receive a new from right */
         MPI_Sendrecv_replace(A, n_local*n_local, MPI_DOUBLE, j_dst,
@@ -179,18 +197,18 @@ int main(int argc, char* argv[])
     double avg_t;
     
     //end timing point
-    //use reduction to collect the final time
+    //use reduction to collect the final timy_rank
     if (coords[2] == 0) 
     {
         MPI_Reduce(&t1, &avg_t, 1, MPI_DOUBLE, MPI_SUM, root, dim_ij_comm);
-        avg_t /= (double) (p_gridsize*p_gridsize);
+        avg_t /= (double) (nproc_ij*nproc_ij);
         
-        if (me == root) printf("[mmm25D]P=%d, N=%d, C=%d, Time=%.9f\n", p, n, c, avg_t);
+        if (my_rank == root) printf("[mmm25D]nproc=%d, N=%d, C=%d, Time=%.9f\n", nproc, n, c, avg_t);
     }
     
     #ifdef VERIFY
     gather_result(
-        root, me, coords, n, p_gridsize, n_local,
+        root, my_rank, coords, n, nproc_ij, n_local,
         dim_ij_comm, sendcounts, displs, subarrtype,
         C, M, NT, P
     );
